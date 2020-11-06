8c3515c527b77c8875b6a66730d8d629eb1a9032
laveberry <berry1990@naver.com>
2020-11-06 18:01:29 +0900

    Conflict(충돌)예상됨

--- src/common/Person.java
+++ src/common/Person.java
@@ -3,9 +3,14 @@ package common;
 public class Person {
 	String name;
 	int age;
+	String nickName;
+	
 	public Person(String name, int age) {
 		super();
 		this.name = name;
 		this.age = age;
 	}
+	
+	
+	
 }
